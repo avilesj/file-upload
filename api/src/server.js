a8fcd12522ed562157738671d880b378fd89f3ce
Jose Aviles <jlapmail@gmail.com>
2020-07-01 22:48:24 -0400

    Dockerize webserver

--- api/src/server.js
+++ api/src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { fileUploadController } from "./controllers/file-upload-controller.js";
 
 const app = express();
 app.post('/', fileUploadController);
 
-app.listen(3000, () => {
+app.listen(process.env.API_PORT || 3000, () => {
     console.log("Server online");
 });
