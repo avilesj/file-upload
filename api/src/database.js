5993cccd0c80f07c1bcb80f407f01de27cdcd155
Jose Aviles <jlapmail@gmail.com>
2020-07-01 21:35:59 -0400

    Add env variables for database

--- api/src/database.js
+++ api/src/database.js
@@ -1,11 +1,11 @@
 import pg from "pg";
 const Pool = pg.Pool;
 const databaseConfig = {
-    user: '',
-    host: '',
-    database: '',
-    password: '',
-    port: 5432
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
 }
 
 const connection = new Pool(databaseConfig);
