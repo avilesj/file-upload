77958aaeec9fda191092e68a14dd0d5ede661f9b
Jose Aviles <jlapmail@gmail.com>
2020-07-01 20:28:35 -0400

    Add file upload to S3

--- api/src/controllers/file-upload-controller.js
+++ api/src/controllers/file-upload-controller.js
@@ -4,21 +4,26 @@ import { fileMetadataService } from "../services/file-metadata-service.js";
 import { fileStorageService } from "../services/file-storage-service.js";
 
 export const fileUploadController = async (req, res, next) => {
-  const busboy = new Busboy({ headers: req.headers });
-  req.pipe(busboy);
-  const fileObject = await parseFormData(busboy);
+  try {
+    const busboy = new Busboy({ headers: req.headers });
+    req.pipe(busboy);
+    const fileObject = await parseFormData(busboy);
 
-  if (!fileObject.isPortableExecutable) {
-    return res.sendStatus(200);
-  }
+    if (!fileObject.isPortableExecutable) {
+      return res.sendStatus(200);
+    }
 
-  const fileMetadata = await fileMetadataService.getFileMetadataByHash(fileObject.fileHash);
-  if (fileMetadata) {
-    await fileMetadataService.saveFileDuplicateEvent(fileMetadata.id);
-    return res.sendStatus(200);
-  } else {
-     await fileStorageService.uploadFile(fileObject);
-    await fileMetadataService.saveFileMetadata(fileObject);
-    return res.sendStatus(200);
+    const fileMetadata = await fileMetadataService.getFileMetadataByHash(fileObject.fileHash);
+    if (fileMetadata) {
+      await fileMetadataService.saveFileDuplicateEvent(fileMetadata.id);
+      return res.sendStatus(200);
+    } else {
+      const fileMetadata = await fileMetadataService.saveFileMetadata({ ...fileObject, fileLocation: process.env.AWS_S3_BUCKET });
+      await fileStorageService.uploadFile({ ...fileObject, fileName: fileMetadata.file_name });
+      return res.sendStatus(200);
+    }
+  } catch (e) {
+    console.error(e);
+    return res.sendStatus(500);
   }
 }
--- api/src/services/file-storage-service.js
+++ api/src/services/file-storage-service.js
@@ -1,5 +1,24 @@
-const uploadFile = () => null;
+import aws from "aws-sdk";
+
+const uploadFile = async ({ fileBuffer, fileName }) => {
+  try {
+    const s3 = new aws.S3({
+      accessKeyId: process.env.AWS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+    });
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: fileName,
+      Body: fileBuffer
+    };
+
+    await s3.upload(params).promise();
+
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 export const fileStorageService = {
-    uploadFile,
+  uploadFile,
 }
