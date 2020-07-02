609269dd7932929549fb7ee632b4f8cda8d6d440
avilesj <jlapmail@gmail.com>
2020-07-02 10:23:40 -0400

    feature/dockerize (#4)
    * Dockerize database
    * Dockerize API
    * Dockerize web
    * Integrate docker-compose
    * Improve configurability

--- api/src/database.js
+++ api/src/database.js
@@ -1,11 +1,11 @@
 import pg from "pg";
 const Pool = pg.Pool;
 const databaseConfig = {
-    user: '',
-    host: '',
-    database: '',
-    password: '',
-    port: 5432
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_DB,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
 }
 
 const connection = new Pool(databaseConfig);
--- api/src/server.js
+++ api/src/server.js
@@ -4,6 +4,6 @@ import { fileUploadController } from "./controllers/file-upload-controller.js";
 const app = express();
 app.post('/', fileUploadController);
 
-app.listen(3000, () => {
+app.listen(process.env.API_PORT || 3000, () => {
     console.log("Server online");
 });
