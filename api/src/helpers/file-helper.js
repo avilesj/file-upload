13080fddc9e5011d29b6a8b1f3ae61969299f7f7
Jose Aviles <jlapmail@gmail.com>
2020-07-02 22:49:46 -0400

    Add file streaming. Refactor pending.

--- api/src/helpers/file-helper.js
+++ api/src/helpers/file-helper.js
@@ -1,4 +1,7 @@
+import { v4 as uuid } from "uuid";
 import md5 from "md5-hex";
+import aws from "aws-sdk";
+import crypto from "crypto";
 
 export const isPortableExecutable = (buffer) => {
   const signature = buffer.toString('hex').slice(0, 9);
@@ -8,21 +11,45 @@ export const isPortableExecutable = (buffer) => {
 }
 
 export const parseFormData = (busboyInstance) => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
+    const s3 = new aws.S3({
+      accessKeyId: process.env.AWS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+    });
+
+    const hash = crypto.createHash('sha1');
+    hash.setEncoding('hex');
     const bufferChunks = [];
+    let size = 0;
+    let firstChunkAnalyzed = false;
+
     busboyInstance.on('file', (fieldname, file) => {
+      file.pipe(hash);
+      const params = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: uuid(),
+        Body: file,
+      };
+
+    s3.upload(params).send();
       file.on("data", (data) => {
+        if(!firstChunkAnalyzed) {
+         if(!isPortableExecutable(data)) {
+           file.resume();
+           reject("Invalid file");
+         }
+          firstChunkAnalyzed = true;
+        }
+        size = size + Buffer.byteLength(data);
         bufferChunks.push(data);
       })
     });
 
     busboyInstance.on("finish", () => {
-        const fileBuffer = Buffer.concat(bufferChunks);
+      hash.end();
         resolve({
-            fileBuffer,
-            fileSize: fileBuffer.length,
-            fileHash: md5(fileBuffer),
-            isPortableExecutable: isPortableExecutable(fileBuffer),
+          fileSize: size,
+          fileHash: hash.read(),
         });
     });
   })
