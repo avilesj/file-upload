c1d2288d2e17acef374de5f825c80072bf1cef24
Jose Aviles <jlapmail@gmail.com>
2020-07-02 21:02:26 -0400

    Remove silent error

--- api/src/services/file-storage-service.js
+++ api/src/services/file-storage-service.js
@@ -1,22 +1,17 @@
 import aws from "aws-sdk";
 
 const uploadFile = async ({ fileBuffer, fileName }) => {
-  try {
-    const s3 = new aws.S3({
-      accessKeyId: process.env.AWS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
-    });
-    const params = {
-      Bucket: process.env.AWS_S3_BUCKET,
-      Key: fileName,
-      Body: fileBuffer
-    };
+  const s3 = new aws.S3({
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  });
+  const params = {
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: fileName,
+    Body: fileBuffer
+  };
 
-    await s3.upload(params).promise();
-
-  } catch (e) {
-    console.error(e);
-  }
+  await s3.upload(params).promise();
 }
 
 export const fileStorageService = {
