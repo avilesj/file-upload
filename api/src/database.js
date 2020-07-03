86db80d56e8850635bcf4160b0caed064b209106
Jose Aviles <jlapmail@gmail.com>
2020-07-03 12:48:05 -0400

    Include database refactor

--- api/src/database.js
+++ api/src/database.js
@@ -1,15 +1,14 @@
 import pg from "pg";
 const Pool = pg.Pool;
 const databaseConfig = {
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_DB,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_DB,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
 }
 
 const connection = new Pool(databaseConfig);
 
-export const database = {
-    getConnection: () => connection,
-}
+
+export const getConnection = () => connection;
