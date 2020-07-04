05a83cb0a7fcb8b7289b6e155e3443d9effc8fa8
avilesj <jlapmail@gmail.com>
2020-07-04 01:22:43 -0400

    release/1.0.0 (#13)
    * Add PE format validation
    * Add File streaming
    * Add SHA1 hashing for storing uniqueness
    * Add Duplicate file event registration
    * Add File metadata storage

--- api/src/services/file-storage-service.js
+++ api/src/services/file-storage-service.js
@@ -9,7 +9,7 @@ const s3 = new aws.S3({
   secretAccessKey: process.env.AWS_SECRET_KEY,
 });
 
-export const storeFileInS3 = ({ fileBuffer, fileName }) => {
+const storeFileInS3 = ({ fileBuffer, fileName }) => {
   const params = {
     Key: fileName,
     Body: fileBuffer,
@@ -17,4 +17,44 @@ export const storeFileInS3 = ({ fileBuffer, fileName }) => {
   };
 
   return s3.upload(params).promise();
+}
+
+export const uploadFile = async (file) => new Promise((resolve, reject) => {
+  const hash = crypto.createHash('sha1');
+  const fileName = uuid();
+  let size = 0;
+  let firstChunkAnalyzed = false;
+
+  const s3UploadPromise = storeFileInS3({ fileBuffer: file, fileName });
+  hash.setEncoding('hex');
+  file.pipe(hash);
+  file.on("data", (data) => {
+    if (!firstChunkAnalyzed) {
+      if (!isPortableExecutable(data)) {
+        file.resume();
+        s3UploadPromise.then(() => reject({ message: "Invalid file", fileName }));
+      }
+      firstChunkAnalyzed = true;
+    }
+    size = size + Buffer.byteLength(data);
+  });
+
+  file.on("end", () => {
+    hash.end();
+    s3UploadPromise.then(() => resolve({
+      fileName,
+      fileSize: size,
+      fileHash: hash.read(),
+      fileLocation: process.env.AWS_S3_BUCKET,
+    }));
+
+  });
+});
+
+export const deleteFile = async ({ fileName }) => {
+  const params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+  };
+  return s3.deleteObject(params).promise();
 }
